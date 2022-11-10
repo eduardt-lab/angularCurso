dc5d723bedc2fe6bae01d871b91f69026c05dddc
David Alarcón <eduardt.david@gmail.com>
2022-11-10 11:36:24 -0500

    interpolación de archivos html

--- proyecto001_prueba/src/app/app.component.ts
+++ proyecto001_prueba/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto001_prueba';
+  nombre = 'David Alarcón';
+  edad = 29;
+  email = 'da@gmail.com';
+  sueldos = [1000, 2000, 3000];
+  activo = true;
+
+  esActivo() {
+    if(this.activo)
+      return 'Trabajador Activo';
+    else 
+    return 'trabajador Inactivo';
+  }
+
+  ultimos3Sueldos() {
+    let suma =0;
+    for(let x=0; x<this.sueldos.length; x++)
+      suma+= this.sueldos[x];
+    return suma;
+  }
 }
